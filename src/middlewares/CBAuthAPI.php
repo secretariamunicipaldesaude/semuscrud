8626849e73cfd414db36530192e14fa19b7149ba
bhayron <bhayronklivilan@gmail.com>
2020-11-06 11:49:55 -0300

    first commit

---
+++ src/middlewares/CBAuthAPI.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace semuscrud\crudbooster\middlewares;
+
+use Closure;
+use semuscrud\crudbooster\helpers\CRUDBooster;
+
+class CBAuthAPI
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+
+        CRUDBooster::authAPI();
+
+        return $next($request);
+    }
+}
